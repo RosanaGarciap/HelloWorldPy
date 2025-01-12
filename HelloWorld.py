f24c54f8a547fcc3989cb94983f48a7265fe87c3
RosanaGarciap <97164329+RosanaGarciap@users.noreply.github.com>
2025-01-11 22:29:32 -0400

    Add files via upload
    HelloWorld means to show how to print an easy message of "Hello World" but with three different ways to do it.

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+# Printing Strings with Python
+
+## FIRST METHOD
+# Directly insert between two quotes the phrase you want to print
+print("Hello World")
+
+## SECOND METHOD
+# Use variables to print "Hello World"
+
+var1 = "Hello"
+var2 = "World!"
+
+print(var1, var2)
+
+## THIRD METHOD
+# we can use f-string property for cases when the string is 
+print(f"{var1} {var2}!")
